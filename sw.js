542ce30e8f965505357129223a45c2efc9e165e5
Joel <u7925718@anu.edu.au>
2026-09-03 22:09:19 +1000

    h

--- sw.js
+++ sw.js
@@ -1,29 +1,59 @@
-const CACHE = 'ledger-v1';
-const SHELL = ['./', './index.html', './manifest.json'];
+const CACHE = "ledger-v2";
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
+const SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches
+      .open(CACHE)
+      .then(cache => cache.addAll(SHELL))
+  );
+
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
   );
+
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (e) => {
-  if(e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then((cached) => {
-      const network = fetch(e.request)
-        .then((res) => {
-          if(res && res.ok){ const copy = res.clone(); caches.open(CACHE).then((c) => c.put(e.request, copy)); }
-          return res;
-        })
-        .catch(() => cached);
-      return cached || network;
-    })
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (
+          response &&
+          response.ok &&
+          new URL(event.request.url).origin === location.origin
+        ) {
+          const copy = response.clone();
+
+          caches.open(CACHE).then(cache => {
+            cache.put(event.request, copy);
+          });
+        }
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
